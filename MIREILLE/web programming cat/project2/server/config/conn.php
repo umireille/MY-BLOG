ba778ba0cb7db0681445c6938b68e05f0c67342c
Mimi <umurerwamireille12@gmail.com>
2023-03-29 17:56:48 +0200

    delete

--- MIREILLE/web programming cat/project2/server/config/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-# Establin the connection to the database
-require('db.php');
-$db_conn = new mysqli($DB_HOST, $DB_USER, $DB_PWD, $DB_NAME);
-if($db_conn->connect_error){
-    // echo("Failed to connect to the Server").$db_conn->connect_errno .$db_conn->connect_error;
-} 
--- MIREILLE/web programming cat/project2/server/config/db_close.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$db_conn->close();
